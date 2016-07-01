e94b8304593b4504a4e4f049777385ff76b21c86
mathgunz <amigos182>
2016-06-30 23:13:23 -0300

    adicionado mapa e criado menu

--- app/src/main/java/gym/com/br/mylocalgym/DashBoard.java
+++ app/src/main/java/gym/com/br/mylocalgym/DashBoard.java
@@ -24,14 +24,6 @@ public class DashBoard extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -81,18 +73,23 @@ public class DashBoard extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_buscar) {
 
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_status_exame) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_favoritos) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_treinos) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_saldo) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_recarga) {
 
+        } else if (id == R.id.nav_solicitar_reembolso) {
+
+        } else if (id == R.id.nav_ultimas_academias) {
+
+        } else if (id == R.id.nav_ultimos_treinos) {
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
