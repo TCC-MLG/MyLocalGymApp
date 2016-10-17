ef3336e4c83f680c0d07d798183f3c930f4a7fce
ra.gonz4lez <ra.gonz4lez@gmail.com>
2016-10-17 19:28:35 -0200

    correções zoom

--- app/src/main/java/gym/com/br/mylocalgym/fragments/MapsFragment.java
+++ app/src/main/java/gym/com/br/mylocalgym/fragments/MapsFragment.java
@@ -1,12 +1,15 @@
 package gym.com.br.mylocalgym.fragments;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,22 +17,30 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsFragment extends SupportMapFragment implements OnMapReadyCallback,
         GoogleMap.OnMapClickListener,
         android.location.LocationListener {
-
+    private View mLayout;
     private GoogleMap mMap;
     private LocationManager locationManager;
     private static final String TAG = "";
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION=0;
     private boolean criado=false;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        int off = Settings.Secure.getInt(getContentResolver(), Settings.Secure.LOCATION_MODE);
+//        if(off==0){
+//            Intent onGPS = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//            startActivity(onGPS);
+//        }
         getMapAsync(this);
+
 
     }
 
@@ -71,13 +82,44 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
 
+//            Log.i(TAG, "Checking permission.");
+//            // BEGIN_INCLUDE(camera_permission)
+//            // Check if the Camera permission is already available.
+//            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA)
+//                    != PackageManager.PERMISSION_GRANTED) {
+//                // Camera permission has not been granted.
+//
+//                requestLocationPermission();
+//
+//            } else {
+//
+//                // Camera permissions is already available, show the camera preview.
+//                Log.i(TAG,
+//                        "CAMERA permission has already been granted. Displaying camera preview.");
+//                mMap.setMyLocationEnabled(true);
+//                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//                //  convert the location object to a LatLng object that can be used by the map API
+//                LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+//                // zoom to the current location
+//                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition,16));
+//                handleNewLocation(location);
+//            }
+            // END_INCLUDE(camera_permission)
+
         try {
             if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
                     == PackageManager.PERMISSION_GRANTED) {
                 mMap.setMyLocationEnabled(true);
                 Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                 handleNewLocation(location);
+                //  convert the location object to a LatLng object that can be used by the map API
+                LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+                // zoom to the current location
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition,18));
             } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
                 // Show rationale and request permission.
             }
         }catch (Exception e){
@@ -113,6 +155,7 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
     private void handleNewLocation(Location location) {
 
         MarkerOptions options = new MarkerOptions();
+
         Log.d(TAG, location.toString());
 
         double currentLatitude = location.getLatitude();
@@ -120,11 +163,23 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
         LatLng latLng = new LatLng(currentLatitude, currentLongitude);
 
         if (!criado) {
-            options = new MarkerOptions()
+            Marker marker = mMap.addMarker(new MarkerOptions()
                     .position(latLng)
-                    .title("Eu estou aqui!");
-            mMap.addMarker(options);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+//                  .icon(BitmapDescriptorFactory.fromResource(R.drawable.academia))
+                    .snippet("Just fit")
+                    .flat(true)
+                    .title("Academia"));
+            marker.showInfoWindow();
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
+
+//            options = new MarkerOptions()
+//                    .position(latLng)
+////                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.academia))
+//                    .snippet("Just fit")
+//                    .flat(true)
+//                    .title("Academia");
+//            mMap.addMarker(options);
+//            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
         }else {
             options.position(latLng);
         }
@@ -143,5 +198,36 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
     public void onProviderDisabled(String provider) {
         Toast.makeText(getActivity(), "Provider desabilitado :): "+ provider, Toast.LENGTH_LONG).show();
     }
+
+//    private void requestLocationPermission() {
+//        Log.i(TAG, "Location permission has NOT been granted. Requesting permission.");
+//
+//        // BEGIN_INCLUDE(camera_permission_request)
+//        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+//                Manifest.permission.ACCESS_FINE_LOCATION)) {
+//            // Provide an additional rationale to the user if the permission was not granted
+//            // and the user would benefit from additional context for the use of the permission.
+//            // For example if the user has previously denied the permission.
+//            Log.i(TAG,
+//                    "Displaying location permission rationale to provide additional context.");
+//            Snackbar.make(mLayout, R.string.permission_location_rationale,
+//                    Snackbar.LENGTH_INDEFINITE)
+//                    .setAction(R.string.ok, new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View view) {
+//                            ActivityCompat.requestPermissions(getActivity(),
+//                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                                    MY_PERMISSIONS_REQUEST_LOCATION);
+//                        }
+//                    })
+//                    .show();
+//        } else {
+//
+//            // Location permission has not been granted yet. Request it directly.
+//            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+//                    MY_PERMISSIONS_REQUEST_LOCATION);
+//        }
+//        // END_INCLUDE(Location_permission_request)
+//    }
 
 }
