2c6d88c7810054f3c39c8e9f19e5c55d70eb73f7
ra.gonz4lez <ra.gonz4lez@gmail.com>
2016-10-21 10:56:40 -0200

    corrige login

--- app/src/main/java/gym/com/br/mylocalgym/activitys/SignInActivity.java
+++ app/src/main/java/gym/com/br/mylocalgym/activitys/SignInActivity.java
@@ -188,4 +188,12 @@ public class SignInActivity extends AppCompatActivity {
             getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 }
